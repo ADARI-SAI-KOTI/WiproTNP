0e6e1a1087f2afae3674f0d399a4e83dcb046d3f
ADARI SAI KOTI <24BQ5A0501@vvit.net>
2026-07-04 22:45:48 +0530

    Update P1.java
    Removed example output comments from P1.java.

--- Practice/Language_Basics/P1.java
+++ Practice/Language_Basics/P1.java
@@ -1,9 +1,4 @@
-/* Write a Program that accepts two Strings as command line arguments and generate the output in the required format.
- Example1)
-If the two command line arguments are Wipro and Bangalore then the output generated should be Wipro Technologies Bangalore.
-Example2)
-If the command line arguments are ABC and Mumbai then the output generated should be ABC Technologies Mumbai
-[Note: It is mandatory to pass two arguments in command line]*/
+/* Write a Program that accepts two Strings as command line arguments and generate the output in the required format.*/
 import java.util.*;
 public class P1{
     public static void main(String[] args){
